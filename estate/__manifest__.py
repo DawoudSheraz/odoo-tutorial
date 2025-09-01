75f4b3eb54f8a2ef616fa7c34b989517131bf8d0
Syed Muhammad Dawoud Sheraz Ali <dawoud.sheraz@gmail.com>
2025-09-01 19:30:29 +0500

    feat: add development tutorial progress so far

---
+++ estate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'summary': 'Manage Real Estate listings',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+        'views/estate_property_form_view.xml',
+        'views/estate_property_search_view.xml',
+        'views/estate_property_list_view.xml',
+    ],
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,54 @@
+from odoo import fields, models
+
+class PropertyType(models.Model):
+    _name="estate.property_type"
+    _description="What sort of property"
+
+    name = fields.Char(required=True)
+
+
+class PropertyTag(models.Model):
+    _name="estate.property.tag"
+    _description="Property Tags"
+
+    name = fields.Char(required=True)
+
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property Listing"
+
+    name = fields.Char(help="Property listing title", required=True)
+    description = fields.Text(required=True)
+    active = fields.Boolean(default=True)
+    property_type_id = fields.Many2one("estate.property_type", string="Property Type")
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('offer_received', "Offer Received"),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', "Cancelled")
+        ],
+        default='new'
+    )
+    tag_ids = fields.Many2many('estate.property.tag', string='Tags')
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), months=3))
+    expected_price = fields.Float()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_areas = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection=[('north','North'), ('south','South'), ('east', 'East'), ('west', 'West')]
+    )
+    garden_walled = fields.Boolean()
+    # res.partner links to an org/company/user
+    buyer = fields.Many2one('res.partner', string='Property Buyer', copy=False)
+    # By default, the current user is the salesperson, res.user is meant for system users where backend or portal
+    salesperson = fields.Many2one('res.users', string='Salesperson', default = lambda self: self.env.user)
+
