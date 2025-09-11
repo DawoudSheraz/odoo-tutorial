7118d7ecfc7edd9f2133ffaa3e64cfa96132ebb4
Syed Muhammad Dawoud Sheraz Ali <dawoud.sheraz@gmail.com>
2025-09-11 18:58:08 +0500

    refactor: update invoice flow

--- estate_account/models/inherited_models.py
+++ estate_account/models/inherited_models.py
@@ -10,6 +10,23 @@ logger = logging.getLogger(__name__)
 class InheritedEstateProperty(models.Model):
     _inherit = 'estate.property'
 
+    def _create_invoice_data_dict(self, property, journal, currency, *args):
+        return {
+            'partner_id': property.buyer.id,
+            'move_type': 'out_invoice',  # Customer Invoice
+            'journal_id': journal.id,
+            'currency_id': currency.id,
+        }
+
+    def _create_invoice_data_lines(self, name, price, **kwargs):
+        return {
+            'name': name,
+            'quantity': kwargs.get('quantity', 0),
+            'price_unit': price,
+            # product_id needs to be False for the price unit to appear
+            'product_id': kwargs.get('product_id', False)
+        }
+
     def mark_property_sold(self):
         res = super().mark_property_sold()
 
@@ -20,27 +37,17 @@ class InheritedEstateProperty(models.Model):
 
         for property in self:
             try:
-                self.env['account.move'].create({
-                    'partner_id': property.buyer.id,
-                    'move_type': 'out_invoice',  # Customer Invoice
-                    'journal_id': customer_journal.id,
-                    'currency_id': currency.id,
-                    # product_id needs to be False for the price unit to appear
-                    'invoice_line_ids': [
-                        Command.create({
-                            'name': 'Selling Price Percentage',
-                            'quantity': 0,
-                            'price_unit': property.selling_price * 0.06,
-                            'product_id': False
-                        }),
-                        Command.create({
-                            'name': 'Administration Fee',
-                            'quantity': 0,
-                            'price_unit': 100.0,
-                            'product_id': False
-                        })
-                    ]
-                })
+                invoice_data = self._create_invoice_data_dict(property, customer_journal, currency)
+                invoice_data_lines = [
+                    Command.create(self._create_invoice_data_lines(
+                        'Selling Price Percentage', property.selling_price * 0.06, quantity=1)
+                    ),
+                    Command.create(self._create_invoice_data_lines(
+                        'Administration Fee', 100.0, quantity=1)
+                    )
+                ]
+                invoice_data.update({'invoice_line_ids': invoice_data_lines})
+                self.env['account.move'].create(invoice_data)
             except:
                 logger.exception(f"Unable to create invoice for property {property}")
 
